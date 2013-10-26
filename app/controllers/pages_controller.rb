b35d49a1e9fe44920d09a1bd1f5bea94676ce221
destinyd <destinyd_war@163.com>
2013-10-26 20:25:18 +0800

    page and post show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   def show
     #@page = Page.where(path: params[:path]).first
     @page = Page.find(params[:id])
+    Page.update_counters @page.id, visits: 1
   end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -5,6 +5,7 @@ class Page
   field :path, type: String
   field :status, type: String
   field :published_at, type: Time
+  field :visits, type: Integer, default: 0
   belongs_to :category
 
   scope :recent, desc(:published_at_at)
