154f811fe4cf8c1770498c39eb7248882a4a2187
destinyd <destinyd_war@163.com>
2013-10-28 00:39:35 +0800

    post prev next

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,18 @@ class Post < Page
 
   validates :title, presence: true
 
+  scope :newer_by, lambda{|post| where(:created_at.gt => post.created_at).older}
+  scope :older_by, lambda{|post| where(:created_at.lt => post.created_at).recent}
+
   def to_s
     title
+  end
+
+  def older
+    @older ||= category.posts.older_by(self).first if category
+  end
+
+  def newer
+    @newer ||= category.posts.newer_by(self).first if category
   end
 end
