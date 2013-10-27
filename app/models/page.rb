dc0168a4a022c9dbd09afd23c7e8fd94035cc4f3
destinyd <destinyd_war@163.com>
2013-10-27 21:40:17 +0800

    admin post

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,7 +2,7 @@ class Page
   include Mongoid::Document
   include Mongoid::Timestamps
   field :content, type: String
-  field :path, type: String
+  #field :path, type: String
   field :status, type: String
   field :published_at, type: Time
   field :visits, type: Integer, default: 0
