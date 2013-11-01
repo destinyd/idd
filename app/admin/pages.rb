87e611ce9c0dc8900aecfff3f4e13a4e762f38bd
destinyd <destinyd_war@163.com>
2013-11-01 09:56:52 +0800

    page and post

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -1,8 +1,9 @@
 ActiveAdmin.register Page do
+  menu label: proc{I18n.t("mongoid.models.page")}
   form do |f|
     f.inputs do
-      f.input :content, as: :kindeditor#, owner_id: page
       f.input :category, collection: Category.all
+      f.input :content, as: :kindeditor#, owner_id: page
     end
     f.actions
   end 
--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Post do
-
+  menu label: proc{I18n.t("mongoid.models.post")}
+  form do |f|
+    f.inputs do
+      f.input :category, collection: Category.all
+      f.input :title
+      f.input :introduction
+      f.input :content, as: :kindeditor#, owner_id: page
+      f.input :image
+    end
+    f.actions
+  end
 end
