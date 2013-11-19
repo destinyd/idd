23862bd77e3bc985887e080acb889689fc54081e
destinyd <destinyd_war@163.com>
2013-11-19 23:04:49 +0800

    flash ord

--- app/models/flash.rb
+++ app/models/flash.rb
@@ -2,9 +2,13 @@ class Flash
   include Mongoid::Document
   include Mongoid::Timestamps
   field :url,              :type => String, :default => ""
+  field :ord, type: Integer, default: ->{ Flash.count + 1}
   #acts_as_commentable
   #acts_as_taggable
   mount_uploader :image, FlashUploader
 
-  scope :recent,desc(:created_at).limit(5)
+  scope :default_order,desc(:ord)
+  scope :recent, default_order.limit(5)
+
+  index({ ord: 1 }, {background: true })
 end
